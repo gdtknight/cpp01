11da9a8530efb0b512684ee081b7c43558ce04b2
yoshin <yoshin@student.42gyeongsan.kr>
2025-11-05 20:45:40 +0900

    fix(ex05): correct complain level strings to match case-sensitive levels

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 16:12:03 by yoshin            #+#    #+#             */
-/*   Updated: 2025/10/27 16:12:03 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/11/05 18:57:07 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,19 @@
 int main(void) {
   Harl harl;
 
-  std::cout << "----- Debug Level -----" << std::endl;
+  std::cout << "----- Debug Level -----" << std::endl << std::endl;
+  harl.complain("DEBUG");
   std::cout << std::endl;
-  harl.complain("debug");
+
+  std::cout << "----- Info Level -----" << std::endl << std::endl;
+  harl.complain("INFO");
   std::cout << std::endl;
-  std::cout << "----- Info Level -----" << std::endl;
+
+  std::cout << "----- Warning Level -----" << std::endl << std::endl;
+  harl.complain("WARNING");
   std::cout << std::endl;
-  harl.complain("info");
-  std::cout << std::endl;
-  std::cout << "----- Warning Level -----" << std::endl;
-  std::cout << std::endl;
-  harl.complain("warning");
-  std::cout << std::endl;
-  std::cout << "----- Error Level -----" << std::endl;
-  std::cout << std::endl;
-  harl.complain("error");
+
+  std::cout << "----- Error Level -----" << std::endl << std::endl;
+  harl.complain("ERROR");
   return (EXIT_SUCCESS);
 }
